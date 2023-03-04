7b99648a48506021a08cedf25fd5d7673bed265f
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-03-04 18:09:15 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome to Cricbuzz World";
+        return "I am Abhay Singh";
     }
 
 }
